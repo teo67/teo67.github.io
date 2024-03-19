dd06abfd7ca0988562bfd05aeef5b0ceb1233626
Theo Gerst <teogerst@gmail.com>
2024-03-18 20:48:24 -0400

    updates updates updates

--- path/main.js
+++ path/main.js
@@ -8,6 +8,13 @@ const redSupply = document.getElementById("red-supply");
 const greenSupply = document.getElementById("green-supply");
 const linesHolder = document.getElementById("lines");
 const weights = document.getElementById("weights");
+const grid = document.getElementById("grid");
+const radialDisplays = document.getElementById("radial-displays");
+const radialOutlines = document.getElementById("radial-outlines");
+const generateButton = document.getElementById("generate");
+const generateAngularDeviation = document.getElementById("path-angular-deviation");
+const generateLinearDeviation = document.getElementById("path-linear-deviation");
+const clearButton = document.getElementById("clear");
 let boatX = 0;
 let boatY = 0;
 const boatRadius = 75/2;
@@ -21,7 +28,15 @@ class Buoy {
         this.element = element;
         updatePosition(this, x, y, radius);
         updateRelativePosition(this);
+        this.radialDisplay = null;
     }
+}
+
+const makeBuoyElement = isRed => {
+    const newElement = document.createElement('div');
+    newElement.classList.add('draggable');
+    newElement.classList.add(isRed ? 'red' : 'green');
+    return newElement;
 }
 
 const updatePosition = (obj, x, y, radius) => {
@@ -47,7 +62,7 @@ const boat = {
     element: boatElement
 };
 
-const buoyList = [];
+let buoyList = [];
 
 let nextRedBuoy = null;
 let nextGreenBuoy = null;
@@ -103,6 +118,14 @@ document.addEventListener('mouseup', () => {
                 buoyList.splice(buoyList.indexOf(selected), 1);
             }
             updateRelativePosition(selected);
+            for(const buoy of buoyList) {
+                if(buoy.radialDisplay !== null) {
+                    for(const display of buoy.radialDisplay) {
+                        display.remove();
+                    }
+                    buoy.radialDisplay = null;
+                }
+            }
         }
         selected = null;
         getBuoyOrderings();
@@ -110,9 +133,7 @@ document.addEventListener('mouseup', () => {
 });
 
 const regenSupply = isRed => {
-    const newElement = document.createElement('div');
-    newElement.classList.add('draggable');
-    newElement.classList.add(isRed ? 'red' : 'green');
+    const newElement = makeBuoyElement(isRed);
     (isRed ? redSupply : greenSupply).appendChild(newElement);
     const newBuoy = new Buoy(isRed, newElement, isRed ? 2 * buoyRadius : 35 + 2 * buoyRadius, 2 * buoyRadius, buoyRadius);
     if(isRed) {
@@ -124,6 +145,27 @@ const regenSupply = isRed => {
 
 let stepping = false;
 
+const makeRadialDisplayIndividual = (x, y, radius, classname, outline = true) => {
+    const newEl = document.createElement("div");
+    newEl.classList.add(outline ? "radial-outline" : "radial-display");
+    newEl.classList.add(classname);
+    newEl.style.top = `${y - radius}px`;
+    newEl.style.left = `${x - radius}px`;
+    newEl.style.width = `${radius * 2}px`;
+    newEl.style.height = `${radius * 2}px`;
+    (outline ? radialOutlines : radialDisplays).appendChild(newEl);
+    return newEl;
+}
+
+const makeFullRadialDisplay = (x, y, sameColor) => {
+    return [
+        makeRadialDisplayIndividual(x, y, (sameColor ? sameColorMaxDist : diffColorMaxDist)*pixelsPerFoot, sameColor ? "same" : "diff", false),
+        makeRadialDisplayIndividual(x, y, (sameColor ? sameColorMaxDist : diffColorMaxDist)*pixelsPerFoot, "outer"),
+        makeRadialDisplayIndividual(x, y, (sameColor ? sameColorMidpointDist : diffColorMidpointDist)*pixelsPerFoot, "center"),
+        makeRadialDisplayIndividual(x, y, (sameColor ? sameColorMinDist : diffColorMinDist)*pixelsPerFoot, "inner")
+    ]
+}
+
 document.addEventListener('mousemove', ev => {
     if(selected !== null) {
         updatePosition(selected, ev.pageX - offsetX, ev.pageY - offsetY, selected == boat ? boatRadius : buoyRadius);
@@ -132,6 +174,25 @@ document.addEventListener('mousemove', ev => {
                 selected.element.classList.add("removing");
             } else if(selected.element.classList.contains("removing")) {
                 selected.element.classList.remove("removing");
+            }
+            for(const buoy of buoyList) {
+                if(buoy == selected) continue;
+                const sameColor = (buoy.isRed == selected.isRed);
+                const maxRadius = (sameColor ? sameColorMaxDist : diffColorMaxDist)*pixelsPerFoot;
+                if(magnitude(buoy.x - selected.x, buoy.y - selected.y) <= maxRadius) {
+                    if(buoy.radialDisplay == null) {
+                        console.log("making display");
+                        buoy.radialDisplay = makeFullRadialDisplay(buoy.x, buoy.y, sameColor);
+                    }
+                } else {
+                    if(buoy.radialDisplay !== null) {
+                        console.log("removing display");
+                        for(const display of buoy.radialDisplay) {
+                            display.remove();
+                        }
+                        buoy.radialDisplay = null;
+                    }
+                }
             }
         }
     }
@@ -180,17 +241,13 @@ const getBuoyOrderings = () => {
             allGreenBuoys.add(i);
         }
     }
-    while(allRedBuoys.size > 0 || allGreenBuoys.size > 0) {
-        if(allGreenBuoys.size > 0) {
-            const index = chooseNext(allGreenBuoys, greenOrdering);
-            allGreenBuoys.delete(index);
-            greenOrdering.push(index);
-        }   
-        if(allRedBuoys.size > 0) {
-            const index = chooseNext(allRedBuoys, redOrdering);
-            allRedBuoys.delete(index);
-            redOrdering.push(index);
-        }
+    let result = chooseNext(allGreenBuoys, allRedBuoys, greenOrdering, redOrdering);
+    while(result !== null) {
+        allGreenBuoys.delete(result.green);
+        greenOrdering.push(result.green);
+        allRedBuoys.delete(result.red);
+        redOrdering.push(result.red);
+        result = chooseNext(allGreenBuoys, allRedBuoys, greenOrdering, redOrdering);
     }
 
     linesHolder.innerHTML = "";
@@ -201,8 +258,12 @@ const getBuoyOrderings = () => {
         }
     }
 
+    for(let i = 0; i < greenOrdering.length; i++) {
+        drawLine(buoyList[greenOrdering[i]].x, buoyList[greenOrdering[i]].y, buoyList[redOrdering[i]].x, buoyList[redOrdering[i]].y, 'cross');
+    }
+
     const waypoints = [];
-    for(let i = 0; i < Math.min(greenOrdering.length, redOrdering.length); i++) {
+    for(let i = 0; i < greenOrdering.length; i++) {
         waypoints.push({x: buoyList[greenOrdering[i]].x/2 + buoyList[redOrdering[i]].x/2,
                         y: buoyList[greenOrdering[i]].y/2 + buoyList[redOrdering[i]].y/2});
     }
@@ -239,43 +300,125 @@ const addWeight = (name, defaultValue) => {
     return input;
 }
 
-const distanceWeight = addWeight('same-color-dist', 1);
-const angleWeight = addWeight('inline-angle', 200/Math.PI);
+const pixelsPerFoot = 10;
+const sameColorMinDist = 10;
+const sameColorMaxDist = 20;
+const diffColorMinDist = 6;
+const diffColorMaxDist = 10;
+
+const sameColorMidpointDist = (sameColorMaxDist + sameColorMinDist)/2;
+const diffColorMidpointDist = (diffColorMaxDist + diffColorMinDist)/2;
+
+const gridInterval = 5; //feet
+
+const distanceWeight = addWeight('same-color-dist', 1/5);
+const distanceMaxDeviation = addWeight('MAX', (sameColorMaxDist - sameColorMinDist) * 1.5);
+const angleWeight = addWeight('inline-angle', 3/Math.PI);
+const angleMaxDeviation = addWeight('MAX', Math.PI/2.5);
+const diffColorDistanceWeight = addWeight('diff-color-dist', 1/5);
+const diffColorDistanceMaxDeviation = addWeight('MAX', (diffColorMaxDist - diffColorMinDist) * 1.5);
+const crossAngleWeight = addWeight('cross-section-angle', 3/Math.PI);
+const crossAngleMaxDeviation = addWeight('MAX', Math.PI/2.5);
 
 const angleBetween = (x1, y1, x2, y2) => {
     return Math.acos((x1 * x2 + y1 * y2) / (magnitude(x1, y1) * magnitude(x2, y2)));
 }
 
-const chooseNext = (set, ordering) => {
-    let lowestDist = null;
-    let lowestIndex = -1;
-    const referenceX = ordering.length == 0 ? 0 : buoyList[ordering[ordering.length - 1]].relativeX;
-    const referenceY = ordering.length == 0 ? 0 : buoyList[ordering[ordering.length - 1]].relativeY;
-    let previousVectorX = 0;
-    let previousVectorY = 0;
-    if(ordering.length > 1) {
-        previousVectorX = buoyList[ordering[ordering.length - 1]].relativeX - buoyList[ordering[ordering.length - 2]].relativeX;
-        previousVectorY = buoyList[ordering[ordering.length - 1]].relativeY - buoyList[ordering[ordering.length - 2]].relativeY;
-    }
-    for(const index of set) {
-        const xDiff = buoyList[index].relativeX - referenceX;
-        const yDiff = buoyList[index].relativeY - referenceY;
-        let dist = magnitude(xDiff, yDiff) * distanceWeight.value;
-        if(ordering.length > 1) {
-            const angle = angleBetween(xDiff, yDiff, previousVectorX, previousVectorY);
-            dist += angle * angleWeight.value;
-        }
-        if(lowestDist == null || dist < lowestDist) {
-            lowestDist = dist;  
-            lowestIndex = index;
-        }
-    }
-    return lowestIndex;
+const getReference = ordering => {
+    return {
+        x: ordering.length == 0 ? 0 : buoyList[ordering[ordering.length - 1]].relativeX,
+        y: ordering.length == 0 ? 0 : buoyList[ordering[ordering.length - 1]].relativeY
+    };
 }
 
-const drawLine = (x1, y1, x2, y2, line_class = "line") => {
-    linesHolder.innerHTML += `<svg><line class=${line_class} x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"/></svg>`
+const getSameColorVector = ordering => {
+    return {
+        x: ordering.length < 2 ? 0 : buoyList[ordering[ordering.length - 1]].relativeX - buoyList[ordering[ordering.length - 2]].relativeX,
+        y: ordering.length < 2 ? 0 : buoyList[ordering[ordering.length - 1]].relativeY - buoyList[ordering[ordering.length - 2]].relativeY
+    };
 }
+
+const getWeight = (ordering, index, reference, scv) => {
+    const xDiff = buoyList[index].relativeX - reference.x;
+    const yDiff = buoyList[index].relativeY - reference.y;
+    const distanceFeet = Math.abs(magnitude(xDiff, yDiff)/pixelsPerFoot - (ordering.length == 0 ? 0 : sameColorMidpointDist));
+    if(ordering.length > 0 && distanceFeet > distanceMaxDeviation.value) {
+        return null;
+    }
+    let dist = distanceFeet * distanceWeight.value;
+    if(ordering.length > 1) {
+        const angle = angleBetween(xDiff, yDiff, scv.x, scv.y);
+        if(angle > angleMaxDeviation.value) {
+            return null;
+        }
+        dist += angle * angleWeight.value;
+    }
+    return dist;
+}
+
+const chooseNext = (greenSet, redSet, greenOrdering, redOrdering) => {
+    console.log(`choosing next: ${greenOrdering.length} chosen so far`);
+    let lowestDist = null;
+    let lowestIndexGreen = -1;
+    let lowestIndexRed = -1;
+    const greenRef = getReference(greenOrdering);
+    const redRef = getReference(redOrdering);
+    const greenSCV = getSameColorVector(greenOrdering);
+    const redSCV = getSameColorVector(redOrdering);
+    for(const greenIndex of greenSet) {
+        const greenWeight = getWeight(greenOrdering, greenIndex, greenRef, greenSCV);
+        if(greenWeight == null) {
+            continue;
+        }
+        for(const redIndex of redSet) {
+            const redWeight = getWeight(redOrdering, redIndex, redRef, redSCV);
+            if(redWeight == null) {
+                continue;
+            }
+            const distFeet = Math.abs(magnitude(buoyList[redIndex].relativeX - buoyList[greenIndex].relativeX, buoyList[redIndex].relativeY - buoyList[greenIndex].relativeY)/pixelsPerFoot - diffColorMidpointDist);
+            if(distFeet > diffColorDistanceMaxDeviation.value) {
+                continue;
+            }
+            const diffColorDist = distFeet * diffColorDistanceWeight.value;
+            let crossAngle = 0;
+            if(greenOrdering.length > 0 && redOrdering.length > 0) {
+                crossAngle = angleBetween(redRef.x - greenRef.x, redRef.y - greenRef.y, buoyList[redIndex].relativeX - buoyList[greenIndex].relativeX, buoyList[redIndex].relativeY - buoyList[greenIndex].relativeY) * crossAngleWeight.value;
+                if(crossAngle > crossAngleMaxDeviation.value) {
+                    continue;
+                }
+            }
+            const dist = greenWeight + redWeight + diffColorDist + crossAngle;
+            console.log(`found weight = ${dist} for green=${greenIndex}, red=${redIndex}`);
+            console.log({green: greenWeight, red: redWeight, diff: diffColorDist, cross: crossAngle});
+            if(lowestDist == null || dist < lowestDist) {
+                lowestDist = dist;  
+                lowestIndexGreen = greenIndex;
+                lowestIndexRed = redIndex;
+            }
+        }
+    }
+    return lowestDist == null ? null : {
+        green: lowestIndexGreen, 
+        red: lowestIndexRed
+    };
+}
+
+const drawLine = (x1, y1, x2, y2, line_class = "line", holder = linesHolder) => {
+    holder.innerHTML += `<svg><line class=${line_class} x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}"/></svg>`
+}
+
+const makeGrid = () => {
+    grid.innerHTML = "";
+    for(let i = 0; i < window.innerWidth; i += gridInterval * pixelsPerFoot) {
+        drawLine(i, 0, i, window.innerHeight, "gridline", grid);
+    }
+    for(let i = 0; i < window.innerHeight; i += gridInterval * pixelsPerFoot) {
+        drawLine(0, i, window.innerWidth, i, "gridline", grid);
+    }
+}
+
+makeGrid();
+addEventListener('resize', makeGrid);
 
 const speed = 100; // pixels/sec
 const stepUpdateRate = .01;// seconds
@@ -307,4 +450,57 @@ const step = () => {
     }
 }
 
+const generatePath = () => {
+    const minX = 5*pixelsPerFoot;
+    const minY = 5*pixelsPerFoot;
+    const maxX = window.innerWidth - 5 * pixelsPerFoot;
+    const maxY = window.innerHeight - 5 * pixelsPerFoot;
+
+    let currentX = minX + 10*pixelsPerFoot;
+    let currentY = window.innerHeight/2;
+    let isRed = false;
+    let isFirst = false;
+
+    while(currentX >= minX && currentX <= maxX && currentY >= minY && currentY <= maxY) {
+        const newElement = makeBuoyElement(isRed);
+        const newBuoy = new Buoy(isRed, newElement, currentX, currentY, buoyRadius);
+        buoys.appendChild(newElement);
+        buoyList.push(newBuoy);
+
+        const randAngle = (Math.random() - 0.5) * 2 * generateAngularDeviation.value;
+        if(isFirst) {
+            const randDist = sameColorMidpointDist + (Math.random() - 0.5) * (sameColorMaxDist - sameColorMinDist) * generateLinearDeviation.value;
+            isFirst = false;
+            currentX += randDist*pixelsPerFoot*Math.cos(randAngle);
+            currentY -= randDist*pixelsPerFoot*Math.sin(randAngle);
+        } else {
+            const randDist = diffColorMidpointDist + (Math.random() - 0.5) * (diffColorMaxDist - diffColorMinDist) * generateLinearDeviation.value;
+            
+            currentX += randDist*pixelsPerFoot*Math.sin(randAngle);
+            currentY += randDist*pixelsPerFoot*Math.cos(randAngle) * (isRed ? -1 : 1);
+
+            isRed = !isRed;
+            isFirst = true;
+        }
+    }
+
+    getBuoyOrderings();
+}
+
+const clearBuoys = () => {
+    for(const buoy of buoyList) {
+        buoy.element.remove();
+    }
+    buoyList = [];
+    getBuoyOrderings();
+}
+
 setInterval(step, stepUpdateRate*1000);
+
+generatePath();
+generateButton.onclick = () => {
+    clearBuoys();
+    generatePath();
+}
+
+clearButton.onclick = clearBuoys;
